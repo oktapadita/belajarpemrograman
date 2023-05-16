eb67ca411cc914877d7a74083fb22cfddd5dc673
Okta Padita <oktapadita@gmail.com>
2023-05-16 17:37:41 +0800

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/latihan3.php
@@ -0,0 +1,43 @@
+<?php
+require 'functions.php';
+//tampung ke variabel mahasiswa
+$mahasiswa = query("SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h1>Daftar Mahasiswa</h1>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php
+    $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" alt="" width="60"></td>
+        <td><?= $m['nama']; ?></td>
+        <td>
+          <a href="detail.php?id=<?= $m['id']; ?>" style="text-decoration: none;">lihat detail</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+
+</body>
+
+</html>
